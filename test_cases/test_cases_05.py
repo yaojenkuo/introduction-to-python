6521a5f20cc807f09c1b1240bc86f08f06fa391e
yaojenkuo <tonykuoyj@gmail.com>
2020-07-22 09:43:06 +0800

    update

--- test_cases/test_cases_05.py
+++ test_cases/test_cases_05.py
@@ -11,6 +11,10 @@ class TestDataStructure(unittest.TestCase):
         self.assertEqual(get_release_dates(the_avengers), ('11 April 2012', '25 April 2012'))
     def test_select_bw_hawkeye(self):
         self.assertEqual(select_bw_hawkeye(the_avengers), ('Natasha Romanoff', 'Clint Barton'))
+    def test_tpe_zip_code(self):
+        self.assertEqual(tpe_zip_code('松山區'), '105')
+        self.assertEqual(tpe_zip_code('信義區'), '110')
+        self.assertEqual(tpe_zip_code('中山區'), '104')
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestDataStructure)
 runner = unittest.TextTestRunner(verbosity=2)
