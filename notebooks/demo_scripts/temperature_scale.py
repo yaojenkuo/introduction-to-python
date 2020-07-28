e317772f866f41bbe5379f2880668e5caa558dd0
yaojenkuo <tonykuoyj@gmail.com>
2020-07-28 12:07:11 +0800

    first commit

---
+++ notebooks/demo_scripts/temperature_scale.py
@@ -0,0 +1,8 @@
+def celsius_to_fahrenheit(x):
+    return x * 9/5 + 32
+
+class CelsiusFahrenheit:
+    def c2f(self, x):
+        return x * 9/5 + 32
+    def f2c(self, x):
+        return (x - 32) * 5/9
